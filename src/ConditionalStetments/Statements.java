4f78b57312a283142c8bcf034c7a945b4925b4b6
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-06 10:38:36 +0530

    first commit

---
+++ src/ConditionalStetments/Statements.java
@@ -0,0 +1,38 @@
+package ConditionalStetments;
+
+public class Statements {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int a=10,b=25;
+			if(a>b) {
+				System.out.println("Saiprasad");
+			}
+			else if (a<b) {
+				System.out.println("Kolhapur");
+			}
+	}
+}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
---
+++ src/ConditionalStetments/SwitchStatement.java
@@ -0,0 +1,45 @@
+package ConditionalStetments;
+public class SwitchStatement {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		switch (a) {
+		case 1:
+		System.out.println("January");
+		break;
+		case 2:
+		System.out.println("February");
+		break;
+		case 3:
+		System.out.println("March");
+		break;
+		case 4:
+		System.out.println("April");
+		break;
+		case 5:
+		System.out.println("may");
+		break;
+		case 6:
+		System.out.println("june");
+		break;
+		case 7:
+		System.out.println("july");
+		break;
+		case 8:
+		System.out.println("august");
+		break;
+		case 9:
+		System.out.println("september");
+		break;
+		case 10:
+		System.out.println("october");
+		break;
+		case 11:
+		System.out.println("november");
+		break;
+		case 12:
+		System.out.println("december");
+		break;
+		}	
+	}
+}
